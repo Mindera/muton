348428e7e9bea8287ef76899b9228214e35d17c2
Bruno Palos <bruno.palos@mindera.com>
2015-05-06 08:15:57 +0100

    Fixing grunt bump file option.

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,7 +124,7 @@ module.exports = function(grunt) {
         },
         bump: {
             options: {
-                files: ['package.json'],
+                files: ['package.json', 'bower.json'],
                 updateConfigs: [],
                 commit: true,
                 commitMessage: 'Release v%VERSION%',
